659be5d362bb68b1ffa90bd984cc902c9944d9f7
AlexMihA <virtuoz82virtuoz@gmail.com>
2022-09-23 09:31:27 +0300

    Fisrt task from Lesson 5

--- HomeWorks/Lesson 5/Lesson5_HomeWork_Authorization/Program.cs
+++ HomeWorks/Lesson 5/Lesson5_HomeWork_Authorization/Program.cs
@@ -2,11 +2,17 @@
 
 namespace Lesson5_HomeWork_Authorization
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Hello! Enter password:");
+            } while (Console.ReadLine() != "administrator");
+
+            Console.WriteLine("Autorization as Administrator is successful!");
+
+        }
+    }
 }
