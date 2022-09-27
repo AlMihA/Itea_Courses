0cb6d93181bd30cc80f733700c16eac54924b774
Aleksandr Avramenko <virtuoz82virtuoz@gmail.com>
2022-09-27 19:37:02 +0300

    auth

--- HomeWorks/Lesson 5/Lesson5_HomeWork_Authorization/Program.cs
+++ HomeWorks/Lesson 5/Lesson5_HomeWork_Authorization/Program.cs
@@ -6,13 +6,28 @@ namespace Lesson5_HomeWork_Authorization
     {
         static void Main(string[] args)
         {
-            do
-            {
-                Console.WriteLine("Hello! Enter password:");
-            } while (Console.ReadLine() != "administrator");
+			const string pass = "123";
+			do
+			{
+				Console.WriteLine("Hello! Enter password:");
+				string answer = Console.ReadLine();
+				if (answer == "administrator")
+				{
+					Console.WriteLine("Autorization as Administrator is successful!");
+					break;
+				}
+				else if (answer == pass)
+				{
+					Console.WriteLine("Autorization as User is successful!");
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Password incorrect!");
+				}
 
-            Console.WriteLine("Autorization as Administrator is successful!");
+			} while (true);
 
-        }
+		}
     }
 }
