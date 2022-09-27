932d468235d48d9370f8b755480d3b036892d90a
Aleksandr Avramenko <virtuoz82virtuoz@gmail.com>
2022-09-27 20:22:48 +0300

    Додав коментарі для додатку на Windows формах

--- HomeWorks/Lesson 5/Lesson5_HomeWork_ShotInTarget/MainForm.cs
+++ HomeWorks/Lesson 5/Lesson5_HomeWork_ShotInTarget/MainForm.cs
@@ -38,6 +38,7 @@ namespace Lesson5_HomeWork_ShotInTarget
             DrawTarget();
         }
 
+        //малювання цілі
         private bool DrawTarget()
         {
             int radius = 0;
@@ -63,9 +64,12 @@ namespace Lesson5_HomeWork_ShotInTarget
             }
            
             graph.Clear(Color.White);
+
+            //вісь Х і У
             graph.DrawLine(Pens.Black, x0 - 10 * radius - 30, y0, x0 + 10 * radius + 30, y0);
             graph.DrawLine(Pens.Black, x0, y0 - 10 * radius - 30, x0, y0 + 10 * radius + 30);
 
+            //10 кругів з радіусом кожен раз більше на радіус
             for (int i = 1; i < 11; i++)
                 graph.DrawEllipse(Pens.Black, x0 - radius * i, y0 - radius * i, radius * 2 * i, radius * 2 * i);
             initTarget = true;
@@ -73,21 +77,29 @@ namespace Lesson5_HomeWork_ShotInTarget
 
         }
 
+        //кнопка "Піу" :)
         private void btnShot_Click(object sender, EventArgs e)
         {
+            //якщо мішень не ініціалізована, то намалювати
             if (!initTarget)
                 if (!DrawTarget())
                     return;
 
+            //постріл
             Shot();
         }
 
+        //підрахунок балів
         private int CalcPoints(int x, int y)
         {
 	        int radius = Convert.ToInt32(txtRadius.Text);
             int a2 = (x - x0) * (x - x0);
             int b2 = (y - y0) * (y - y0);
+
+            //якщо гіпотенуза прямокутного трикутника з кутами в центрі та координатами точки менше радіуса, то точка належить цьому колу
             double gipotenuza = Math.Sqrt(a2 + b2);
+
+            //перевірка всех 10ьох радіусів
             for (int i = 1; i < 11; i++)
             {
 	            if (gipotenuza <= radius * i)
@@ -98,16 +110,22 @@ namespace Lesson5_HomeWork_ShotInTarget
             }
 	        return 0;
         }
+        //постріл
         private void Shot()
         {
+            //генерування точки пострілу
             Random rand = new Random();
             int x = rand.Next(TARGET_WIDTH);
             int y = rand.Next(TARGET_HEIGHT);
 
+            //вивід точки з координатами на форму
             labelCurrentShot.Text = "(" + (x - x0).ToString() + "," + (y0-y).ToString() + ")";
+
+            //малювання точки в вигляді кола з дуже маленьким радіусом
             Pen blackPen = new Pen(Color.Red, 3);
             graph.DrawEllipse(blackPen, x, y, 4, 4);
 
+            //підрахунок балів і вивід на форму разом з сумою балів
             int currentPoint = CalcPoints(x, y);
             sumPoints += currentPoint;
             if (currentPoint == 0)
