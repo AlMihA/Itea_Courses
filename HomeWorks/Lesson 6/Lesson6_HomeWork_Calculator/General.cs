b7ff21d700168e89e0c0abc0c6ea0e9f6ef0c50e
AlexMihA <virtuoz82virtuoz@gmail.com>
2022-09-28 17:14:48 +0300

    Start homework 6

---
+++ HomeWorks/Lesson 6/Lesson6_HomeWork_Calculator/General.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson6_HomeWork_Calculator
+{
+    public static class General
+    {
+        public static string[] ActionAndValues()
+        {
+            string[] answer = new string[3];
+            Console.WriteLine("Choose action(+,-,*,/)");
+            while (true)
+            {
+                string _Action = Console.ReadLine();
+                if (_Action == "+" || _Action == "-" || _Action == "*" || _Action == "/")
+                {
+                    answer[0] = _Action;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect action! Try again");
+                }
+            }
+            Console.WriteLine("Enter first number");
+            answer[1] = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            answer[2] = Console.ReadLine();
+
+            return answer;
+        }
+}
+}
---
+++ HomeWorks/Lesson 6/Lesson6_HomeWork_Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson6_HomeWork_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator();
+        }
+
+        static void Calculator()
+        {
+            Console.WriteLine("Choose Number System:");
+            Console.WriteLine("1. Binary");
+            Console.WriteLine("2. Decimal");
+            Console.WriteLine("3. Hexadecimal");
+
+            while (true)
+            {
+                string chooseSystem = Console.ReadLine();
+                switch (chooseSystem)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        DecimalCalc.StartCalc();
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Try again");
+                        break;
+                }
+            }
+        }
+    }
+}
