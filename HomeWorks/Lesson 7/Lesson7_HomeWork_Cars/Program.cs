54ac711ce536059f835f1610e2a7b255fad06763
Aleksandr Avramenko <virtuoz82virtuoz@gmail.com>
2022-10-04 12:21:44 +0300

    Lesson7, homework

---
+++ HomeWorks/Lesson 7/Lesson7_HomeWork_Cars/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lesson7_HomeWork_Cars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello! This is program for car accounting!");
+			Console.WriteLine("How many cars?");
+
+			int count = Convert.ToInt32(Console.ReadLine());
+			string[,] cars = new string[count, 4];
+
+			string owner, brand, number, year, pattern;
+			bool incorrectvalue = true;
+			for (int i = 0; i < count; i++)
+			{
+				incorrectvalue = true;
+				Console.WriteLine("Enter owner([A-Z][a-z] [A-Z][a-z]):");
+				do
+				{
+					pattern = @"^[A-Z][a-z]+\s[A-Z][a-z]+$";
+					owner = Console.ReadLine();
+					if (Regex.IsMatch(owner, pattern, RegexOptions.None))
+					{
+						incorrectvalue = false;
+					}
+					else
+					{
+						Console.WriteLine("Incorrect value! Try again");
+					}
+
+				} while (incorrectvalue);
+
+				incorrectvalue = true;
+				Console.WriteLine("Enter brand:");
+				do
+				{
+					brand = Console.ReadLine();
+					if (Regex.Replace(brand, " ", "") == "")
+					{
+						Console.WriteLine("Incorrect value! Try again");
+					}
+					else
+					{
+						incorrectvalue = false;
+					}
+
+				} while (incorrectvalue);
+
+				incorrectvalue = true;
+				Console.WriteLine("Enter number([A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z][A-Z]):");
+				do
+				{
+					pattern = @"^[A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z][A-Z]$";
+					number = Console.ReadLine();
+					if (Regex.IsMatch(number, pattern, RegexOptions.None))
+					{
+						incorrectvalue = false;
+					}
+					else
+					{
+						Console.WriteLine("Incorrect value! Try again");
+					}
+
+				} while (incorrectvalue);
+				
+				incorrectvalue = true;
+				Console.WriteLine("Enter year:");
+				do
+				{
+					pattern = @"^[1-2][890][0-9][0-9]$";
+					year = Console.ReadLine();
+					if (Regex.IsMatch(year, pattern, RegexOptions.None))
+					{
+						if (Convert.ToInt32(year) < 1886 || Convert.ToInt32(year) > Convert.ToInt32(DateTime.Now.Year))
+						{
+							Console.WriteLine("Incorrect value! Try again");
+						}
+						else
+						{
+							incorrectvalue = false;
+						}
+					}
+					else
+					{
+						Console.WriteLine("Incorrect value! Try again");
+					}
+
+				} while (incorrectvalue);
+
+				cars[i, 0] = owner;
+				cars[i, 1] = brand;
+				cars[i, 2] = number;
+				cars[i, 3] = year;
+			}
+			for (int i = 0; i < cars.GetLength(0); i++)
+			{
+				Console.WriteLine("{0} Car: , owner: {1}, brand: {2}, number {3}, year{4}", i+1, cars[i, 0], cars[i, 1], cars[i, 2], cars[i, 3]);
+
+			}
+		}
+	}
+}
---
+++ HomeWorks/Lesson 7/Lesson7_HomeWork_Employees/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+
+namespace Lesson7_HomeWork_Employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string[,] mainArray = new string[1, 2];
+            string[,] subArray;
+            Console.WriteLine("Hello! This is program for employee accounting!");
+            while (true)
+            {
+                Console.WriteLine("Enter employee:");
+                string employee = Console.ReadLine();
+
+                Console.WriteLine("Enter his salary:");
+                bool incorrectvalue = true;
+                string stringSalary = "";
+
+                do
+                {
+                    stringSalary = Console.ReadLine();
+                    try 
+                    {
+                        double doubleSalary = Convert.ToDouble(stringSalary);
+                        if (doubleSalary<=0 || doubleSalary > 1000000)
+                        {
+                            Console.WriteLine("Incorrect value! Try again");
+                        }
+                        else
+                        {
+                            incorrectvalue = false;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Incorrect value! Try again");
+                    }
+
+                } while (incorrectvalue);
+
+                if (count == 0)
+                {
+	                mainArray[count, 0] = employee;
+	                mainArray[count, 1] = stringSalary;
+	                count += 1;
+                }
+                else
+                {
+	                subArray = new string[count+1, 2];
+                    for (int i = 0; i < count; i++)
+                        for (int j = 0; j < 2; j++)
+	                        subArray[i, j] = mainArray[i, j];
+                    subArray[count, 0] = employee;
+                    subArray[count, 1] = stringSalary;
+                    mainArray = subArray;
+                    count += 1;
+                }
+
+                Console.WriteLine("Do you want to enter another employee y/n");
+                if (Console.ReadKey().KeyChar == 'y')
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    break;
+                }
+            }
+            for (int i = 0; i < mainArray.GetLength(0); i++)
+            {
+                Console.WriteLine("Employee {0}, salary - {1}", mainArray[i, 0], mainArray[i, 1]);
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+}
