d36e605addd459b72ce4fd5c405be52be62948a2
Aleksandr Avramenko <virtuoz82virtuoz@gmail.com>
2022-10-06 19:13:50 +0300

    Lesson 8

---
+++ HomeWorks/Lesson 8/Lesson8_HomeWork_Datetime/DateTime_Tools.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Lesson8_HomeWork_Datetime
+{
+	public class DateTime_Tools
+	{
+		private DateTime date;
+		private bool leapYear;
+
+		public DateTime Date
+		{
+			get
+			{
+				return date;
+			}
+			set
+			{
+				date = value;
+				leapYear = (date.Year % 4 == 0) ? true : false;
+			}
+		}
+
+		public bool LeapYear
+		{
+			get
+			{
+				return leapYear;
+			}
+		}
+		
+		public DateTime_Tools()
+		{
+			this.Date = new DateTime(2000, 1, 1); ;
+		}
+
+		public DateTime_Tools(DateTime d)
+		{
+			this.Date = d;
+		}
+		
+		public DateTime GetPreviousDate()
+		{
+			return Date - TimeSpan.FromDays(1);
+		}
+
+		public DateTime GetNextDate()
+		{
+			return Date + TimeSpan.FromDays(1);
+		}
+
+		public int DaysToEndOfMonth()
+		{
+			return System.DateTime.DaysInMonth(Date.Year, Date.Month) - Date.Day;
+		}
+
+		public override string ToString()
+		{
+			return Date.ToString("d");
+		}
+
+		public string ToString(bool grin)
+		{
+			if (grin)
+				return Date.ToString("R");
+			else
+			{
+				return Date.ToString();
+			}
+		}
+
+		public DateTime this[int i]
+		{
+
+			get
+			{
+				return Date + TimeSpan.FromDays(i);
+			}
+		}
+
+
+
+	}
+}
---
+++ HomeWorks/Lesson 8/Lesson8_HomeWork_Datetime/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lesson8_HomeWork_Datetime
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			do
+			{
+				string d = "";
+				Console.WriteLine("Do you want to set datetime y/n?");
+				if (Console.ReadLine() == "y")
+				{
+					Console.WriteLine("Enter date:");
+					bool incorrectvalue = true;
+					do
+					{
+						try
+						{
+							d = Console.ReadLine();
+							DateTime temp = DateTime.Parse(d);
+							incorrectvalue = false;
+						}
+						catch
+						{
+							Console.WriteLine("Incorrect value! Try again");
+						}
+
+					} while (incorrectvalue);
+
+				}
+
+				DateTime_Tools dtools;
+				if (d != "")
+				{
+					dtools = new DateTime_Tools(Convert.ToDateTime(d));
+				}
+				else
+				{
+					dtools = new DateTime_Tools();
+				}
+
+				Console.WriteLine("Date = " + dtools.ToString());
+				Console.WriteLine("Previous Day = " + dtools.GetPreviousDate().ToString("d"));
+				Console.WriteLine("Next Day = " + dtools.GetNextDate().ToString("d"));
+				Console.WriteLine("Days to end of month = " + dtools.DaysToEndOfMonth());
+
+				Console.WriteLine("Override ToString = " + dtools.ToString());
+				Console.WriteLine("Overload ToString(Grinvich) = " + dtools.ToString(true));
+				Console.WriteLine("Overload ToString(Not Grinvich) = " + dtools.ToString(false));
+
+				Console.WriteLine("Leap year = " + dtools.LeapYear);
+
+				Console.WriteLine("Index(5) = " + dtools[5].ToString("d"));
+				Console.WriteLine("Index(-3) = " + dtools[-3].ToString("d"));
+
+				dtools.Date = DateTime.Now;
+				Console.WriteLine("Change date on today = " + dtools.Date.ToString("d"));
+
+			} while (true);
+
+		}
+	}
+}
