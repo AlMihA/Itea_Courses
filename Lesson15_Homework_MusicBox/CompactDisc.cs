bcbaa3b63fd823ea3291ccb07a7605a66ef452d1
AlexMihA <virtuoz82virtuoz@gmail.com>
2022-11-03 15:58:06 +0200

    L15_4

--- Lesson15_Homework_MusicBox/CompactDisc.cs
+++ Lesson15_Homework_MusicBox/CompactDisc.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace Lesson15_Homework_MusicBox
 {
-	public class CompactDisc
-	{
-		public ArrayList SongsList;
-		public int Number;
+    public class CompactDisc
+    {
+        public ArrayList SongsList;
 
-		public CompactDisc(int number)
-		{
-			this.Number = number;
-			SongsList = new ArrayList();
-		}
-	}
+        public CompactDisc()
+        {
+            SongsList = new ArrayList();
+        }
+
+        public void ShowDisc()
+        {
+            for (int i = 0; i < SongsList.Count; i++)
+            {
+                Song song = (Song)SongsList[i];
+
+                Console.WriteLine("\t" + (i + 1) + ". " + song.Artist + " " + song.Title);
+            }
+        }
+
+    }
 }
--- Lesson15_Homework_MusicBox/MusicBox.cs
+++ Lesson15_Homework_MusicBox/MusicBox.cs
@@ -3,30 +3,173 @@ using System.Collections.Generic;
 
 namespace Lesson15_Homework_MusicBox
 {
-	public class MusicBox
-	{
-		Dictionary<int, CompactDisc> colCompactDiscs = new Dictionary<int, CompactDisc>();
+    public class MusicBox
+    {
+        Dictionary<string, CompactDisc> colCompactDiscs = new Dictionary<string, CompactDisc>();
 
-		private void InitCollection()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				CompactDisc cd = new(i);
-				for (int j = 0; j < 15; j++)
-				{
-					cd.SongsList.Add(new Song("Artist" + j, "Song" + j));
-				}
-				colCompactDiscs.Add(i, cd);
-			}
-		}
+        private void InitCollection()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                CompactDisc cd = new();
+                for (int j = 0; j < 15; j++)
+                {
+                    cd.SongsList.Add(new Song("Artist" + j, "Song" + i + j));
+                }
+                colCompactDiscs.Add("Disc " + i, cd);
+            }
+        }
 
-		public void Start()
-		{
-			InitCollection();
-			Console.WriteLine("Choose operation:");
-		}
+        public void ShowCollection()
+        {
+            foreach (KeyValuePair<string, CompactDisc> currentCD in colCompactDiscs)
+            {
+                Console.WriteLine(currentCD.Key);
+                currentCD.Value.ShowDisc();
+            }
+        }
+        public void Start()
+        {
+            InitCollection();
+            bool shomMenu = true;
+            do
+            {
+
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Choose operation:");
+                Console.WriteLine("1. Show collection");
+                Console.WriteLine("2. Show disc by name");
+                Console.WriteLine("3. Add disc to collection");
+                Console.WriteLine("4. Remove disc from collection");
+                Console.WriteLine("5. Add song to disc");
+                Console.WriteLine("6. Remove song from disc by index");
+                Console.WriteLine("7. Search songs by artist");
+                Console.WriteLine("8. Exit");
+
+                string operation = Console.ReadLine();
+                string discName;
+                switch (operation)
+                {
+                    case "1":
+                        ShowCollection();
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter disc name");
+                        discName = Console.ReadLine();
+                        if (colCompactDiscs.ContainsKey(discName))
+                        {
+                            colCompactDiscs[discName].ShowDisc();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong disc name");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter disc name");
+                        discName = Console.ReadLine();
+                        if (!colCompactDiscs.ContainsKey(discName))
+                        {
+                            CompactDisc cd = new();
+                            colCompactDiscs.Add(discName, cd);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Disc is already exists");
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter disc name");
+                        discName = Console.ReadLine();
+                        if (colCompactDiscs.ContainsKey(discName))
+                        {
+                            colCompactDiscs.Remove(discName);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong disc name");
+                        }
+                        break;
+                    case "5":
+                        Console.WriteLine("Enter disc name");
+                        discName = Console.ReadLine();
+                        if (colCompactDiscs.ContainsKey(discName))
+                        {
+                            Console.WriteLine("Enter artist");
+                            string artist = Console.ReadLine();
+                            if (!(artist == ""))
+                            {
+                                Console.WriteLine("Enter title");
+                                string title = Console.ReadLine();
+                                if (!(title == ""))
+                                {
+                                    colCompactDiscs[discName].SongsList.Add(new Song(artist, title));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Wrong title");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong artist");
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong disc name");
+                        }
+                        break;
+                    case "6":
+                        Console.WriteLine("Enter disc name");
+                        discName = Console.ReadLine();
+                        if (colCompactDiscs.ContainsKey(discName))
+                        {
+                            int countCD = colCompactDiscs[discName].SongsList.Count;
+                            if (countCD == 0)
+                            {
+                                Console.WriteLine("No songs on disc");
+                                break;
+                            }
+                            Console.WriteLine("Enter index from 1 to "+ countCD);
+                            int songIndex;
+                            try
+                            {
+                                songIndex = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Wrong index");
+                                break;
+                            }
+                            
+                            if (songIndex > countCD)
+                            {
+                                Console.WriteLine("Wrong index");
+                                break;
+                            }
+
+                            colCompactDiscs[discName].SongsList.RemoveAt(songIndex-1);
+                            
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong disc name");
+                        }
+                        break;
+                    case "8":
+                        shomMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation. Try again");
+                        break;
+                }
+            } while (shomMenu);
+        }
 
 
 
-	}
+    }
 }
